f096f53d0d8a259a9d9d0b424c2f421c40a36c51
ali said <alisaid_ali@yahoo.com>
2022-04-12 12:29:25 -0700

    functions

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,18 @@
 #include "main.h"
-
 /**
  * test_islower - Test the _islower function
  *
  * @n: Number to pass to _islower function
  */
+void test_islower(int n)
+{
+	int r;
+
+	r = _islower(n);
+	_putchar(r + '0');
+	_putchar('\n');
+}
+
 /**
  * main - check the code for Holberton School students.
  *
@@ -12,15 +20,6 @@
  */
 int main(void)
 {
-	int r;
-
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);	
-
+	test_islower('H');
+	return (0);
 }
