1570f2ebdbfbb4666d7128eeb946dc6a556463af
ali said <alisaid_ali@yahoo.com>
2022-04-13 16:45:17 -0700

    Testing

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void testIt()
+{
+	int n;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
+
+	return (0);
+}
