5ae5349851086cb2f5d06f1db9258e17aae3650f
ali said <alisaid_ali@yahoo.com>
2022-04-14 13:56:41 -0700

    Primes

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,32 +1,15 @@
-/* C Program to Find Prime Factors of a Number using For Loop */
+/* C Program to find the factor of a Number using a Function */
  
 #include <stdio.h>
- 
-int main()
-{
-  	int i, j, Number, isPrime; 
-   
-  	printf("\n Please Enter any number to Find Factors :  ");
-  	scanf("%d", &Number);
- 
-  	for (i = 2; i <= Number; i++)
-   	{
-     	if(Number % i == 0)
-        {
-   			isPrime = 1;
-			for (j = 2; j <= i/2; j++)
-			{
-				if(i % j == 0)
-				{
-					isPrime = 0;
-					break;
-				}
-			} 
-			if(isPrime == 1)
-			{
-				printf("%d is a Prime Factor\n", i);
-			}	          	
-		}
-   }
-  	return 0;
+int main() {
+    int num, i;
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+    printf("Factors of %d are: ", num);
+    for (i = 1; i <= num; ++i) {
+        if (num % i == 0) {
+            printf("%d ", i);
+        }
+    }
+    return 0;
 }
