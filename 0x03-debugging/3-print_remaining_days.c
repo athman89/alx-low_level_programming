f00dc011a5a4ada1e2b33bed0c8854b628419188
ali said <alisaid_ali@yahoo.com>
2022-04-14 03:42:54 -0700

    days of our lives

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,7 +12,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-    if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
+if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
     {
         if (month >= 2 && day >= 60)
         {
@@ -24,7 +24,7 @@ void print_remaining_days(int month, int day, int year)
     }
     else
     {
-        if (month == 0 && day == 60)
+        if (month == 0 && day == 0)
         {
             printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
         }
@@ -34,5 +34,6 @@ void print_remaining_days(int month, int day, int year)
             printf("Remaining days: %d\n", 365 - day);
         }
     }
+    
 }
 
