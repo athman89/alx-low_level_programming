ffba0b989e27701aa31e88f66dbfc8367b025ee1
ali said <alisaid_ali@yahoo.com>
2022-04-14 17:20:19 -0700

    Primes

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,27 +1,51 @@
-/* C Program to find the factor of a Number using a Function */
- 
+/* C Program to find largest prime*/
+/* factor of number*/
+#include <math.h>
 #include <stdio.h>
  
-void primeFactors(unsigned long n)
+
+unsigned long maxPrimeFactors(unsigned long n)
 {
-  unsigned long c = 2;
-  while (n > 1) {
-    if (n % c == 0) {
-      printf("%lu", c);
-      printf("\n");
-      n /= c;
+    
+    unsigned long maxPrime = -1;
+    int i;
+    
+    while (n % 2 == 0) {
+        maxPrime = 2;
+        n >>= 1; 
     }
-    else
-      c++;
-  }
+    
+    while (n % 3 == 0) {
+        maxPrime = 3;
+        n = n / 3;
+    }
+ 
+    
+    for ( i = 5; i <= sqrt(n); i += 6) {
+        while (n % i == 0) {
+            maxPrime = i;
+            n = n / i;
+        }
+        while (n % (i + 2) == 0) {
+            maxPrime = i + 2;
+            n = n / (i + 2);
+        }
+    }
+ 
+     
+    if (n > 4)
+        maxPrime = n;
+ 
+    return maxPrime;
 }
  
-/* Driver code */
+
 int main()
 {
-  unsigned long n;
-  printf("Enter any number to print Prime factors: ");
-    scanf("%lu", &n);
-  primeFactors(n);
-  return 0;
+    unsigned long n;
+    printf("Enter a number: \n");
+    scanf("lu\n",&n);
+    printf("The largest factor is: \n", maxPrimeFactors(n));
+ 
+    return 0;
 }
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_number(int n)
+{
+	_putchar(n);
+	_putchar("\n");
+}
