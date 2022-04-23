4380183057137803e5d9ed4cccb2843ceb967280
ali said <alisaid_ali@yahoo.com>
2022-04-23 13:08:00 -0700

    two strings together

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * copy - Entry point
+ * @s: pointed destination
+ * @b: constant byte
+ * @n: bytes
+ * Return: Always 0 (Success)
+ */
+char *_strcat(char *dest, char *src)
+{
+	int length = 0,j;
+	while (dest[length] != '\0') 
+	{
+	++length;
+	}
+	for (j = 0; src[j] != '\0'; ++j, ++length)
+	{
+	dest[length] = src[j];
+	}
+	dest[length] = '\0';
+	_putchar(dest);
+}
