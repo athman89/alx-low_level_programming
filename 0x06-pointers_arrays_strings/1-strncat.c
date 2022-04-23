79aa9c287bbb54087ae8de65fe0db95e9e4214e0
ali said <alisaid_ali@yahoo.com>
2022-04-23 14:21:38 -0700

    another comming together

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
  * strncat - Entry point
  * @dest: input
@@ -5,10 +6,20 @@
  * @n: input
  * Return: Always 0 (Success)
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src,int n)
 {
+	int length = 0;
+	int dest_len;
 	int i;
-	for (i = 0; i < n; i++)
-	dest[i] = src[i];
-	return (dest);
+	while(dest[length]!='\0')
+	{
+	++length;
+	}
+	dest_len = length;
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+	{
+	dest[dest_len + i] = src[i];
+	}
+	dest[dest_len + i] = '\0';
+	return dest;
 }
