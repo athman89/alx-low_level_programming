50de2f4d67a39a6f0909ba036c8a00efaa1c4be0
ali said <alisaid_ali@yahoo.com>
2022-04-23 13:45:17 -0700

    two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,12 +5,10 @@
  * @n: input
  * Return: Always 0 (Success)
  */
-char *_strncat(char *dest, char *src, unsigned int n)
+char *_strncat(char *dest, char *src, int n)
 {
-	unsigned int i;
-
+	int i;
 	for (i = 0; i < n; i++)
-		dest[i] = src[i];
-
+	dest[i] = src[i];
 	return (dest);
 }
