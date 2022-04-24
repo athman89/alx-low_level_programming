4ab47f86119daf3fd58c435f22abc8fc52d8cee2
ali said <alisaid_ali@yahoo.com>
2022-04-24 00:41:35 -0700

    a comparison

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,6 +8,9 @@ int _strcmp(char *s1, char *s2)
 {
 	const unsigned char *p1 = ( const unsigned char * )s1;
 	const unsigned char *p2 = ( const unsigned char * )s2;
-	while ( *p1 && *p1 == *p2 ) ++p1, ++p2;
+	while ( *p1 && *p1 == *p2 )
+	{
+	++p1, ++p2;
+	}
 	return ( *p1 > *p2 ) - ( *p2  > *p1 );
 }
