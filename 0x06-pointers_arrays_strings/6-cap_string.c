9a23afb50fb0d6137d531f1c26f5442940242541
ali said <alisaid_ali@yahoo.com>
2022-04-24 05:02:38 -0700

    capitalize it

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+char *cap_string(char *)
+{
+	char *str;
+	for(i=0; str[i]!='\0'; i++)
+	{
+	if(i==0)
+	{
+	if((str[i]>='a' && str[i]<='z'))
+	str[i]=str[i]-32;
+	continue; 
+	}
+	if(str[i]==' ')
+	{
+	++i;
+	if(str[i]>='a' && str[i]<='z')
+	{
+	str[i]=str[i]-32; 
+	continue; 
+	}
+	}
+	else
+	{
+	if(str[i]>='A' && str[i]<='Z')
+	str[i]=str[i]+32; 
+	}
+	} 
+	return (str);
+}
+
+}
