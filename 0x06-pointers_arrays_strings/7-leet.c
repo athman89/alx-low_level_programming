e7939550b1c0227ed4d1545d6fee0de0867495fc
ali said <alisaid_ali@yahoo.com>
2022-04-24 05:27:09 -0700

    encoding it

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,7 +9,6 @@ char *leet(char *n)
 	int i, x;
 	int find[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 	int replacer[] = {'4', '3', '0', '7', '1'};
-
 	for (i = 0; n[i] != '\0'; i++)
 	{
 		for (x = 0; x <= 9; x++)
@@ -21,6 +20,5 @@ char *leet(char *n)
 			}
 		}
 	}
-
 	return (n);
 }
