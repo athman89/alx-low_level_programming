8953dd902ce8aa7c34626b9ccd2dc510817f3f9c
ali said <alisaid_ali@yahoo.com>
2022-04-24 01:07:25 -0700

    a reversal

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - check the code
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+void reverse_array(int *a, int n)
+{
+	int temp;
+	while (start < end)
+	{
+	temp = arr[start];  
+	arr[start] = arr[end];
+	arr[end] = temp;
+	start++;
+	end--;
+	}
+}
