fb2db969884bbaa5f8337fdbf7250fefd4d05b56
ali said <alisaid_ali@yahoo.com>
2022-04-26 00:47:29 -0700

    locating a character

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -32,4 +32,5 @@ char *cap_string(char *);
 char *leet(char *n);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 #endif
