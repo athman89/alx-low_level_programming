613f7cbb9218a2242774f2cd1d349e768e3dbc71
ali said <alisaid_ali@yahoo.com>
2022-05-05 14:15:19 -0700

    to infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,10 +11,8 @@
 int main(int argc, char *argv[])
 {
 	int i, sum = 0;
-
 	if (argc < 1)
 		return (0);
-
 	for (i = 1; i < argc; i++)
 	{
 		if (!atoi(argv[i]))
@@ -25,6 +23,5 @@ int main(int argc, char *argv[])
 		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
-
 	return (0);
 }
