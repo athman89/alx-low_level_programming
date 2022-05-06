0baab62620e3c435c56c0f8f83062ac90550b415
ali said <alisaid_ali@yahoo.com>
2022-05-06 12:32:08 -0700

    a womans imagination

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -38,5 +38,6 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 #endif
-
