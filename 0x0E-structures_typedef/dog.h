98ac60bedd8853a6f0155ba78497e1e333575568
ali said <alisaid_ali@yahoo.com>
2022-05-12 09:50:41 -0700

    first struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,12 +7,12 @@
  * @age: age
  * @owner: owner
  */
-	typedef struct dog
-	{
+ typedef struct dog
+ {
 	char *name;
 	float age;
 	char *owner;
-	} dog_t;
+ } dog_t;
 
 	void init_dog(struct dog *d, char *name, float age, char *owner);
 	void print_dog(struct dog *d);
