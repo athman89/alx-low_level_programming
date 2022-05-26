5afa9a50aaf2b261c9b72f007bf894ac4c5f8fc1
ali said <alisaid_ali@yahoo.com>
2022-05-26 04:27:08 -0700

    add it anywhere you want

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - Entry Point
+ * @head: head
+ * @idx: index
+ * @n: data
+ * Return: 0
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int num = 0;
+
+	listint_t *new_node;
+	listint_t *temp;
+
+	if (*head == NULL)
+	{
+		return (NULL);
+	}
+	temp = *head;
+
+	while (temp != NULL)
+	{
+		if (num == idx - 1)
+		{
+			new_node = malloc(sizeof(listint_t));
+			if (new_node == NULL)
+			{
+				return (NULL);
+			}
+			else
+			{
+				new_node->n = n;
+				new_node->next = temp->next;
+				temp->next = new_node;
+				return (new_node);
+			}
+		}
+		if (temp->next != NULL)
+			temp = temp->next;
+		num++;
+	}
+	return (NULL);
+}
